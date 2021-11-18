aafcb88235096e1bb7b5a595f4d49f854eedbe41
bronwynrayfield <bronwyn.rayfield@apexrms.com>
2021-11-18 09:17:32 -0500

    Create bundle_pkg.cmd
    Command prompt call to bundle package

---
+++ bundle_pkg.cmd
@@ -0,0 +1,6 @@
+
+set PACKAGE_NAME=stconnect
+set PACKAGE_VERSION_NAME=stconnect-1-1-15
+set SSIM_DIR="C:\Program Files\SyncroSim"
+
+%SSIM_DIR%\SyncroSim.PackageManager.exe --create=C:\Users\bronw\Documents\Apex\Projects\%PACKAGE_NAME%\src --file=%PACKAGE_VERSION_NAME%.ssimpkg --force
